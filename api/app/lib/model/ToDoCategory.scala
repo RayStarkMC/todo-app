9e027fb53cee38941c74cf65dd6ff57b3c251fe7
yuma.otani <yuma.otani@nextbeat.net>
2024-09-30 16:31:31 +0900

    カテゴリ色をEnum化

--- api/app/lib/model/ToDoCategory.scala
+++ api/app/lib/model/ToDoCategory.scala
@@ -1,6 +1,8 @@
 package lib.model
 
 import ixias.model._
+import ixias.util.EnumStatus
+import lib.model.ToDoCategory.Color
 
 import java.time.LocalDateTime
 
@@ -10,10 +12,17 @@ case class ToDoCategory(
   createdAt: LocalDateTime = NOW,
   name:      String,
   slug:      String,
-  color:     Short
+  color:     Color
 ) extends EntityModel[ToDoCategory.Id]
 
 object ToDoCategory {
   type Id = Long @@ ToDoCategory
   val Id = the[Identity[Id]]
+
+  abstract class Color(val code: Short, val rgb: String) extends EnumStatus
+  object Color extends EnumStatus.Of[Color] {
+    case object Type1 extends Color(code = 1, rgb = "#ffe4b5")
+    case object Type2 extends Color(code = 2, rgb = "#00ffff")
+    case object Type3 extends Color(code = 3, rgb = "#7fffd4")
+  }
 }
