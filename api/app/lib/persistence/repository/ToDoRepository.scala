461efb546b760a02a53e6a2b225606a1c12df789
yuma.otani <yuma.otani@nextbeat.net>
2024-10-03 15:34:37 +0900

    ToDoRepository#getById定義

--- api/app/lib/persistence/repository/ToDoRepository.scala
+++ api/app/lib/persistence/repository/ToDoRepository.scala
@@ -14,6 +14,13 @@ class ToDoRepository @Inject() (
   @Named("master") master: Database,
   @Named("slave") slave:   Database,
 )(implicit val ec:         ExecutionContext) extends SlickRepository[ToDo.Id, ToDo] {
+  def getById(id: ToDo.Id): Future[Option[ToDo#EmbeddedId]] = {
+    val query = ToDoTable.query.filter(_.id === id)
+    val dbio = query.result.headOption
+
+    slave.run(dbio).map(_.map(_.toEmbeddedId))
+  }
+
   def add(user: ToDo#WithNoId): Future[ToDo.Id] = {
     val dbio = ToDoTable.query returning ToDoTable.query.map(_.id) += user.v
     master.run(dbio)
