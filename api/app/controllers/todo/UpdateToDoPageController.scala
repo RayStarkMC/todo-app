462263e9a80a6cbfb3b59ec9f34264ef535f9a96
yuma.otani <yuma.otani@nextbeat.net>
2024-10-04 13:45:47 +0900

    編集画面移行時に編集対象の情報が初期表示されるように変更

--- api/app/controllers/todo/UpdateToDoPageController.scala
+++ api/app/controllers/todo/UpdateToDoPageController.scala
@@ -1,34 +1,57 @@
 package controllers.todo
 
+import cats.data.OptionT
 import lib.AsyncMessagesInjectedController
 import lib.model.ToDo
-import lib.usecase.query.GetAllCategoryOptionsQuery
+import lib.usecase.query.UpdateToDoPageQuery
+import lib.usecase.query.UpdateToDoPageQuery.Input
 import model.ViewValueUpdateToDo
-import play.api.mvc.{ Action, AnyContent }
+import model.form.todo.UpdateToDoForm
+import play.api.mvc.{Action, AnyContent, Result}
 
-import javax.inject.{ Inject, Singleton }
+import javax.inject.{Inject, Singleton}
 
 @Singleton
 class UpdateToDoPageController @Inject() (
-  query: GetAllCategoryOptionsQuery
+  query: UpdateToDoPageQuery
 ) extends AsyncMessagesInjectedController {
   def action(id: Long): Action[AnyContent] = Action.async { implicit request =>
-    for {
-      output <- query.run()
-    } yield {
-      val vv = ViewValueUpdateToDo(
-        id              = id,
-        categoryOptions = output.options.map { option =>
-          option.id.toString -> option.name
-        },
-        statusOptions   = ToDo.Status.values.map { status =>
-          status.code.toString -> (status match {
-            case ToDo.Status.TO_DO       => "ToDo"
-            case ToDo.Status.IN_PROGRESS => "InProgress"
-            case ToDo.Status.DONE        => "Done"
-          })
-        }
-      )
+    val input = Input(id = ToDo.Id(id))
+
+    val optionT =
+      for {
+        output <- OptionT(query.run(input))
+        toDo    = output.toDo
+        options = output.options
+        mapping = UpdateToDoForm.form.mapping
+      } yield {
+        ViewValueUpdateToDo(
+          id              = toDo.id,
+          updateToDoForm  = UpdateToDoForm.form.bind(
+            mapping.unbind(
+              UpdateToDoForm(
+                title    = toDo.title,
+                body     = toDo.body,
+                category = toDo.categoryId,
+                status   = toDo.state.code
+              )
+            )
+          ),
+          categoryOptions = options.map { option =>
+            option.id.toString -> option.name
+          },
+          statusOptions   = ToDo.Status.values.map { status =>
+            status.code.toString -> (status match {
+              case ToDo.Status.TO_DO       => "ToDo"
+              case ToDo.Status.IN_PROGRESS => "InProgress"
+              case ToDo.Status.DONE        => "Done"
+            })
+          }
+        )
+      }
+    optionT.fold[Result](
+      NotFound
+    ) { vv =>
       Ok(views.html.UpdateToDo(vv))
     }
   }
